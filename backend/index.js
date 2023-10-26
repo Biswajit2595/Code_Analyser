b13559d35c33ab42f3769eb0bcc8251edb50b9f3
Biswajit Roy <broy16431@gmail.com>
2023-10-26 23:04:27 +0530

    Code Analyser

---
+++ backend/index.js
@@ -0,0 +1,106 @@
+const express=require("express")
+const cors=require("cors")
+const axios=require("axios")
+const app=express()
+require("dotenv").config()
+app.use(cors())
+app.use(express.json())
+const PORT=process.env.PORT
+
+app.get("/",(req,res)=>{
+    res.send("Code Converter,code Debugger,Code Quality Analysis")
+})
+
+app.post("/convert", async (req, res) => {
+    const { code, language } = req.body;
+    try {
+        const response = await axios.post("https://api.openai.com/v1/chat/completions", {
+            model:'gpt-3.5-turbo',
+            messages:[
+                { role:'system',content:`Analyse the given code and convert the code in ${language}` },
+                { role:'user',content:code}
+            ],
+            temperature: 1,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+            }
+        });
+        let convertedCode=response.data.choices[0].message.content
+        res.status(200).send(convertedCode);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send({ "error": error.message });
+    }
+});
+
+app.post("/quality", async (req, res) => {
+    const { code } = req.body;
+    try {
+        const response = await axios.post("https://api.openai.com/v1/chat/completions",{
+            model:'gpt-3.5-turbo',
+            messages:[
+                { role:'system',content:`act as experienced developer in different domain. your task is to check the quality of the code. Provide a comprehensive code quality assessment, rating each of these parameters from 1 to 10: Code Consistency, Code Performance, Code Documentation, Error Handling, Code Testability, Code Complexity, Code Duplication, and Code Readability. Summarize the overall assessment with higher ratings indicating better quality and lower ratings highlighting areas for improvement.` },
+                { role:'user',content:code }
+            ],
+            temperature: 1,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+            }
+        });
+
+        res.status(200).send({"data":response.data.choices[0].message.content});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send({ "error": error.message });
+    }
+});
+
+app.post("/debug", async (req, res) => {
+    const { code } = req.body;
+    try {
+        const response = await axios.post("https://api.openai.com/v1/chat/completions",{
+            model:'gpt-3.5-turbo',
+            messages:[
+                { role:'system',content:`Act as an seasoned software developer, your responsibility is to identify and rectify any existing issues within the provided code. Subsequently, you are expected to furnish an enhanced version of the code. Please elucidate the specific problem that you encountered and elaborate on the precise steps taken to resolve it.` },
+                { role:'user',content:code }
+            ],
+            temperature: 1,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+            }
+        });
+        res.status(200).send({"data":response.data.choices[0].message.content});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send({ "error": error.message });
+    }
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running at ${PORT}`)
+})
+
+
+// for(let a=0;a<4;a++)
+// {
+//     console.log(a)
+// }
---
+++ frontend/src/App.jsx
@@ -0,0 +1,170 @@
+import { Box, Button, Flex, Heading, Select, useToast } from '@chakra-ui/react';
+import { Editor } from '@monaco-editor/react';
+import ReactMarkdown from 'react-markdown';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+
+
+
+function App() {
+  const toast=useToast()
+  const inputRef = useRef(null);
+  const [code, setCode] = useState('// Enter Your Code Here');
+  const [convertedCode, setConvertedCode] = useState('Your Output Will Be Shown Here');
+  const [language, setLanguage] = useState('');
+
+  const inputFocus = () => {
+    if (inputRef.current) {
+      inputRef.current.editor.focus();
+    }
+  };
+  useEffect(()=>{
+    inputFocus()
+  })
+
+  const handleDebug = () => {
+    if(code==='// Enter Your Code Here'){
+      toast({
+        position:"top",
+        title: 'No Code found',
+        description: "You haven't provided any code for me to review and enhance.Please provide some code and try Again",
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+    }else{
+    
+    axios.post(`https://code-converter-api-n1mc.onrender.com/debug`,{code}).then((res)=>{
+      console.log(res)
+      setConvertedCode(res.data)
+    }).catch((error)=>{
+      console.log(error)
+      setConvertedCode("A network error occured, please try again!")
+    })
+  }
+  };
+
+  const handleConvert = () => {
+    if(code==='// Enter Your Code Here'){
+      toast({
+        position:"top",
+        title: 'No Code found',
+        description: "You haven't provided any code to convert.Please provide some code and try Again",
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+    }else{
+    
+  
+    axios.post(`https://code-converter-api-n1mc.onrender.com/convert`,{code,language}).then((res)=>{
+      console.log(res)
+      setConvertedCode(res.data)
+      setLanguage("")
+    }).catch((error)=>{
+      console.log(error)
+      setConvertedCode("A network error occured, please try again!")
+     
+    })
+  }
+  };
+  
+  
+
+  const handleQuality = () => {
+    if(code==='// Enter Your Code Here'){
+      toast({
+        position:"top",
+        title: 'No Code found',
+        description: "You haven't provided any code for me to do quality Check.Please provide some code and try Again",
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+    }else{
+    
+    axios.post(`https://code-converter-api-n1mc.onrender.com/quality`,{code}).then((res)=>{
+      setConvertedCode(res.data)
+    }).catch((error)=>{
+      console.log(error.message)
+      setConvertedCode("A network error occured, please try again!")
+     
+    })
+  }
+  };
+  const handleInput=(val)=>{
+    setCode(val)
+  }
+
+
+  return (
+      <Box w="100vw" margin="auto">
+        <Heading backgroundColor={"gray.300"} textAlign="center" p={2}>CODE ANALYSER</Heading>
+      <Flex bg="gray.600" justifyContent="space-around" p={5} boxShadow="md">
+        <Select
+          width="30%"
+          color={"gray.900"}
+          fontWeight={700}
+          value={language}
+          onChange={(e) => setLanguage(e.target.value)}
+        >
+          <option value="">SELECT LANGUAGE</option>
+          <option value="javascript">JavaScript</option>
+          <option value="python">Python</option>
+          <option value="java">Java</option>
+          <option value="c++">C++</option>
+        </Select>
+        <Button
+          colorScheme="telegram"
+          variant="solid"
+          isDisabled={!language}
+          onClick={handleConvert}
+          _hover={{ background: "#f9d015",color:"black" }}
+        >
+          CONVERT
+        </Button>
+        <Button
+          colorScheme="telegram"
+          variant="solid"
+          _hover={{ background: "#f9d015",color:"black" }}
+          onClick={handleDebug}
+        >
+          DEBUG
+        </Button>
+        <Button
+          colorScheme="telegram"
+          variant="solid"
+          _hover={{ background: "#f9d015",color:"black" }}
+          onClick={handleQuality}
+        >
+          QUALITY CHECK
+        </Button>
+      </Flex>
+      <Flex bg="gray.200">
+        <Box width="50%" bg={'blackAlpha.600'} p={1.5}>
+          <Editor
+            ref={inputRef}
+            value={code}
+            height="90vh"
+            width="100%"
+            mode="javascript"
+            theme="vs-dark"
+            enableBasicAutocompletion={true}
+            enableLiveAutocompletion={true}
+            enableSnippets={true}
+            editorProps={{ $blockScrolling: true }}
+            onChange={handleInput}
+          />
+        </Box>
+        <Box  width="50%" color={"#fff"} bg={"blackAlpha.700"} p={5}>
+          <h2 style={{ color: "white", fontWeight: "600" }}>Output</h2>
+          <ReactMarkdown>{convertedCode}</ReactMarkdown>
+        </Box>
+      </Flex>
+      </Box>
+  );
+}
+
+
+
+export default App;
